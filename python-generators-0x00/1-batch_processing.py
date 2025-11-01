9487c2593ef873afa8837a29ccd1e0d4791829e3
keroojoseph <keroojoseph0@gmail.com>
2025-11-01 16:50:35 +0200

    Batch processing Large Data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def stream_users_in_batches(batch_size):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",           
+            password="SecureP@sw0rd!123",   
+            database="ALX_prodev"
+        )
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute (
+                f"SELECT * FROM user_data LIMIT {batch_size}"
+            )
+        
+        for row in cursor:
+            yield row
+            
+    except Error as e:
+        print(e)
+
+
+def batch_processing(batch_size):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",           
+            password="SecureP@sw0rd!123",   
+            database="ALX_prodev"
+        )
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute (
+                f"SELECT * FROM user_data WHERE age > 25 LIMIT {batch_size}"
+            )
+        
+        for row in cursor:
+            yield row
+            
+    except Error as e:
+        print(e)
