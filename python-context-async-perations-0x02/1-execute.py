75ae4d35949c87cdaf6651fa13aef32f181197f6
keroojoseph <keroojoseph0@gmail.com>
2025-11-09 12:35:05 +0200

    Add SQL statement

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -5,7 +5,8 @@ class ExecuteQuery:
     def __init__(self, connection, query, paramter):
        self.connect = connection
        cursor = self.connect.cursor()
-       cursor.execute(query, (paramter))
+       result = cursor.execute(query, (paramter))
+       return result
        
     def __enter__(self):
         return self.connect
@@ -13,3 +14,12 @@ class ExecuteQuery:
     def __exit__(self, exc_type, exc_value, traceback):
         return self.connect.close()
     
+    
+with connect(
+    host = 'localhost',
+    database = 'users',
+    password = 'testpassword',
+    port = 3306,
+    user = 'root'
+) as connection:
+    connection_db = ExecuteQuery(connection, 'SELECT * FROM users WHERE age > %s', 25)
