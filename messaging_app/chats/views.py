65a4fc330c9586b1bbe4916a52227c0bd29fd2ba
keroojoseph <keroojoseph0@gmail.com>
2025-11-13 18:32:50 +0200

    Build api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
 # Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all().prefetch_related('participants', 'messages')
+    serializer_class = ConversationSerializer
+    
+class MessageiewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().select_related('sender', 'conversation')
+    serializer_class = MessageSerializer
+    
