66ae83f8c12489ff882228d1a032ce127e9de1c7
keroojoseph <keroojoseph0@gmail.com>
2025-11-13 18:54:53 +0200

    Add messages_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -5,6 +5,8 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('message', views.MessageViewSet, basename='message')
 router.register('conversation', views.ConversationViewSet, basename='conversation')
+messages_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+
 
 app_name = 'chats'
 
