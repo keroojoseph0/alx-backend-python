60965ba95c04e605de568fbee9456ae57bbce3ed
keroojoseph0 <keroojoseph2@.com>
2025-12-21 23:20:59 +0200

    Use Docker Compose for Multi-Container Setup

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,8 +114,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'PORT': os.getenv('MYSQL_PORT'),
+        'HOST': os.getenv('MYSQL_HOST'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'USER': os.getenv('MYSQL_USER')
     }
 }
 
